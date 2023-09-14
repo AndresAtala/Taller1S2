6e093cf3c097ec76181e89c1703bc711d8dd1e35
AndresAtala <132239849+AndresAtala@users.noreply.github.com>
2023-09-14 10:49:32 -0300

    Add files via upload

--- src/main/java/Package/PartidoBaloncecto.java
+++ src/main/java/Package/PartidoBaloncecto.java
@@ -1,11 +1,41 @@
 package Package;
 
-public class PartidoBaloncecto extends Partido {
+public class PartidoBaloncesto extends Partido {
 
 	private int tipoCancha;
 	private String TiemposJugados;
 
-	public PartidoBaloncecto(String equipoOponente, String resultado, String fecha, String lugarPartido) {
+	public PartidoBaloncesto(String equipoOponente, String resultado, String fecha, String lugarPartido) {
 		super(equipoOponente, resultado, fecha, lugarPartido);
+	}
+
+	// Getter para tipoCancha
+	public int getTipoCancha() {
+		return tipoCancha;
+	}
+
+	// Setter para tipoCancha
+	public void setTipoCancha(int tipoCancha) {
+		this.tipoCancha = tipoCancha;
+	}
+
+	// Getter para TiemposJugados
+	public String getTiemposJugados() {
+		return TiemposJugados;
+	}
+
+	// Setter para TiemposJugados
+	public void setTiemposJugados(String tiemposJugados) {
+		TiemposJugados = tiemposJugados;
+	}
+
+	public static void main(String[] args) {
+		PartidoBaloncesto partido = new PartidoBaloncesto("EquipoOponente", "Resultado", "Fecha", "Lugar");
+		partido.setTipoCancha(1);
+		partido.setTiemposJugados("2 tiempos");
+
+		int tipoCancha = partido.getTipoCancha();
+		String tiemposJugados = partido.getTiemposJugados();
+
 	}
 }
