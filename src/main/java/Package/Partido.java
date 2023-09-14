e0a3c2b7b526dd3b2a7c86f0af27d7cf7b84a832
AndresAtala <132239849+AndresAtala@users.noreply.github.com>
2023-09-14 09:43:59 -0300

    Add files via upload
    getters and setters y constructor

--- src/main/java/Package/Partido.java
+++ src/main/java/Package/Partido.java
@@ -2,9 +2,48 @@ package Package;
 
 public class Partido {
 
-	private string EquipoOponente;
-	private string resultado;
-	private string fecha;
-	private string lugarPartido;
+	private String equipoOponente;
+	private String resultado;
+	private String fecha;
+	private String lugarPartido;
+	
+	public Partido(String equipoOponente, String resultado, String fecha, String lugarPartido) {
+		this.equipoOponente = equipoOponente;
+		this.resultado = resultado;
+		this.fecha = fecha;
+		this.lugarPartido = lugarPartido;
+	}
 
+	public String getEquipoOponente() {
+		return equipoOponente;
+	}
+
+	public void setEquipoOponente(String equipoOponente) {
+		this.equipoOponente = equipoOponente;
+	}
+
+	public String getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(String resultado) {
+		this.resultado = resultado;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getLugarPartido() {
+		return lugarPartido;
+	}
+
+	public void setLugarPartido(String lugarPartido) {
+		this.lugarPartido = lugarPartido;
+	}
 }
+
